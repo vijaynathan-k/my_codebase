8179fa0760be9ed7cae82decf3180a434ffd9be2
vijaynathan-k <vijaynathan.k@gmail.com>
2026-02-17 10:55:24 +0530

    Added Multiply with default arguments

--- projects/cpp_with_pybind11/basics_of_pybind11/basic_export.cpp
+++ projects/cpp_with_pybind11/basics_of_pybind11/basic_export.cpp
@@ -11,6 +11,11 @@ int substract(int i, int j)
     return i - j;
 }
 
+int multiply_with_default_args(int i = 2, int j = 3)
+{
+    return i*j;
+}
+
 // 2. Create the Python module
 // "fast_math" is the name you will use in Python: 'import fast_math'
 PYBIND11_MODULE(fast_math, m) {
@@ -19,4 +24,16 @@ PYBIND11_MODULE(fast_math, m) {
     // Expose the 'add' function to Python
     m.def("add", &add, "A function that adds two integers");
     m.def("substract", &substract, "A function to subtract two numbers", py::arg("i"), py::arg("j"));
+    m.def("multiply", &multiply_with_default_args, "A function to multiply two numbers", py::arg("i")=2, py::arg("j")=3);
+
+    /*
+    To expose a value from C++, use the attr function to register it in a module as shown below.
+    Built-in types and general objects (more on that later) are automatically converted when assigned as attributes,
+    and can be explicitly converted using the function py::cast.
+    */
+    m.attr("int_value") = 42;
+
+    // casting the string obj to py object; then putting it in obj_world_str
+    py::object string_obj = py::cast("world");
+    m.attr("string_obj") = string_obj;
 }
--- projects/cpp_with_pybind11/basics_of_pybind11/test_it.py
+++ projects/cpp_with_pybind11/basics_of_pybind11/test_it.py
@@ -2,8 +2,16 @@ import fast_math
 
 result_add = fast_math.add(10, 5)
 result_substract = fast_math.substract(10,5)
+result_multiply_with_default_args = fast_math.multiply()
+result_multiply_with_custom_args = fast_math.multiply(5,10)
 
 print(f"Result from C++ Add: {result_add}")
 print(f"Type of result_add: {type(result_add)}")
 print(f"Result from C++ Substract: {result_substract}")
+print(f"Result with default args from C++ multiply: {result_multiply_with_default_args}")
+print(f"Result with custom args from C++ multiply: {result_multiply_with_custom_args}")
 
+
+print("exporting int : ", fast_math.int_value)
+
+print("exporting complex obj like string : ", fast_math.string_obj)
