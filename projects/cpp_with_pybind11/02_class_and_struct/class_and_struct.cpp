652dff1cdaf1888ed2a921582290a8e050c19f28
vijaynathan-k <vijaynathan.k@gmail.com>
2026-02-18 18:16:26 +0530

    Binding class with arg, as value, reference and pointer

--- projects/cpp_with_pybind11/02_class_and_struct/class_and_struct.cpp
+++ projects/cpp_with_pybind11/02_class_and_struct/class_and_struct.cpp
@@ -1,7 +1,19 @@
 #include <string>
 #include "pybind11/pybind11.h"
+#include <iostream>
 
 namespace py = pybind11;
+
+class dummy{
+    public:
+        dummy() = default;
+        ~dummy() = default;
+    
+        void print_something()
+        {
+            std::cout << "print something!" << std::endl;
+        }
+};
 
 struct Pet {
     Pet(const std::string &name) : name(name) { }
@@ -9,6 +21,53 @@ struct Pet {
     const std::string &getName() const { return name; }
 
     std::string name;
+};
+
+class class_with_value_arg{
+public:
+    explicit class_with_value_arg(Pet p)
+    : m_pet{p}
+    {        
+    }
+    void print_pet_name()
+    {
+        std::cout << m_pet.getName() << std::endl;
+    }
+private:
+    Pet m_pet;
+};
+
+class class_with_reference_arg{
+public:
+    class_with_reference_arg(Pet& p)
+        : m_pet(p)
+    {
+    }
+
+    void print_pet_name()
+    {
+        std::cout << "class_with_reference_arg, pet name : " << m_pet.getName() << std::endl;
+    }
+private:
+    Pet& m_pet;
+};
+
+class class_with_raw_ptr{
+    public:
+        class_with_raw_ptr(Pet* p)
+            : m_pet{p}
+        {            
+        }
+
+        void print_pet_name()
+        {
+            if(!m_pet)
+                std::cout << "class_with_raw_ptr, m_pet is null!" << std::endl;
+
+            std::cout << "class_with_raw_ptr, pet name : " << m_pet->getName() << std::endl;
+        }
+private:
+    Pet* m_pet;
 };
 
 /*
@@ -21,4 +80,20 @@ PYBIND11_MODULE(class_and_struct, m){
         .def(py::init<const std::string &>())
         .def("setName", &Pet::setName)
         .def("getName", &Pet::getName);
+
+    py::class_<dummy>(m, "dummy")
+        .def(py::init()) //default constructor
+        .def("print_something", &dummy::print_something);
+    
+    py::class_<class_with_value_arg>(m, "class_with_value_arg")
+        .def(py::init<Pet>())
+        .def("print_pet_name", &class_with_value_arg::print_pet_name);
+
+    py::class_<class_with_reference_arg>(m, "class_with_reference_arg")
+        .def(py::init<Pet&>())
+        .def("print_pet_name", &class_with_reference_arg::print_pet_name);
+    
+    py::class_<class_with_raw_ptr>(m, "class_with_raw_ptr")
+        .def(py::init<Pet *>())
+        .def("print_pet_name", &class_with_raw_ptr::print_pet_name);
 }
--- projects/cpp_with_pybind11/02_class_and_struct/test_it.py
+++ projects/cpp_with_pybind11/02_class_and_struct/test_it.py
@@ -2,3 +2,15 @@ import class_and_struct as cas
 
 p = cas.Pet("Molly")
 print(p.getName())
+
+dmy = cas.dummy()
+dmy.print_something()
+
+obj = cas.class_with_value_arg(p)
+obj.print_pet_name()
+
+ref = cas.class_with_reference_arg(p)
+ref.print_pet_name()
+
+obj = cas.class_with_raw_ptr(p)
+obj.print_pet_name()
