fde6be7fd921fdb74131ca31c559e8556f85dd83
vijaynathan-k <vijaynathan.k@gmail.com>
2026-02-18 20:15:59 +0530

    Accessing public instance data members

---
+++ projects/cpp_with_pybind11/03_accessing_instance_and_static_data_members/class_and_struct.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include "pybind11/pybind11.h"
+#include <iostream>
+
+namespace py = pybind11;
+
+struct Pet {
+    Pet(const std::string &name) 
+        :   name(name),
+            m_initial_name {"CONST VALUE"}
+    {
+    }
+    
+    void setName(const std::string &name_) { name = name_; }
+    const std::string &getName() const { return name; }
+
+    std::string name;
+    const std::string m_initial_name;
+    inline static std::string STATIC_NAME {"static string"};
+};
+
+/*
+py::class_ creates bindings for a C++ class or struct-style data structure. 
+init() is a convenience function that takes the types of a constructor’s parameters as template arguments 
+and wraps the corresponding constructor
+*/
+PYBIND11_MODULE(class_and_struct, m){
+    py::class_<Pet>(m, "Pet")
+        .def(py::init<const std::string &>())
+        .def_readwrite("name",&Pet::name);
+}
---
+++ projects/cpp_with_pybind11/03_accessing_instance_and_static_data_members/test_it.py
@@ -0,0 +1,5 @@
+import class_and_struct as cas
+
+p = cas.Pet("Molly")
+p.name = "setting the instance variable"
+print(p.name)
